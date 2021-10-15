23ab5ce9357fe518bd2618c22be6b7d558acc955
A01252618 <A01252618@itesm.mx>
2021-10-15 17:42:56 +0000

    examenparte1

--- assignments/22InversoDigitos/src/exercise.py
+++ assignments/22InversoDigitos/src/exercise.py
@@ -1,6 +1,19 @@
 def main():
     num = int(input("Enter a number: "))
     #escribe tu código abajo de esta línea
+    contador = len(str(num))
+
+    if contador<=6:
+        if num>0:
+            n=int(str(num)[::-1])
+            print(n)
+        elif num<0:
+            numero=num*-1
+            n=int(str(numero)[::-1])
+            m=n*-1
+            print(m)
+    else:
+        print('Too long')
     
 
 
